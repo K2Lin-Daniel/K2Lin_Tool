f3d906c53d6d6a7f01160928795298cd6123bf2c
K2Lin <77517478+K2Lin-Daniel@users.noreply.github.com>
2025-03-01 11:52:06 +0800

    v11 update + horizontal recoil suppression

---
+++ K2Lin Tools V11.lua
@@ -0,0 +1,454 @@
+--------------------		[[ K2LINGROUP Universal No-Recoil Script ]]			--------------------
+------------------------------------------------		Settings			--------------------------------------------------
+local Master    = true				------ Masterswitch status defaults to at script start.   		[ true = on, false = off ]	Default = off
+local debug = true 				------ See Extra Function Details. 									Default = false
+local masterswitch = 13				------ Button to toggle main switch, set it to 13 to disable it						Default = 7	
+local modeswitch = 11				------ Button to switch modes										Default = 9			
+local Recoil = true 				------ This is what the recoil defaults to at script start.   		[ true = on, false = off ]	Default = off
+local Automatic = false				------ This is what the rapid-fire defaults to at script start.		[ true = on, false = off ]	Default = off
+local Y_level = 3				------ Vertical move per step. 										Default = 1(=<0 to work)
+local Delay = 21 				------ Default delay on startup. 									Default = 1			
+local mode_change_button = 10 			------ Button to cycle the config									Default = 8					
+local mode_change_enabled = true		------ This allows you switch between configs								Default = true
+local mode_selected = 1 			------ Default config selected upon loading the script							Default = 1
+local rapid_Y_level = 10			------ Rapid-Fire Recoil Settings 									Default = 4-10
+local rapid_delay = 50				------ Rapid-Fire Delay Settings (standing alone)							Default = 10-20
+local rapid_firerate = 200			------ Rapid-Fire Click Speed (Lower = Faster) changeable in config					Default = 100-800
+local rapid_button = 1				------ Button that activates Rapid fire function(Press with RMB)					Default = 5
+local autofirekey = 82				------ Binds to your in-game Secondary Fire Key to use autofire func					Default = 82 (Scan code)
+local x_composite_enabled = true		------ WIP horizontal recoil suppression								Default = true
+local reset_button = 9				------ Button that will reset config slot to #1								Default = 9
+local autotag = false				------ Auto spot enemies when you're firing			[ true = on, false = off ]		Default = true
+local tagbutton = "z"				------ "Spot" key in game										Default = z
+local ReleseDate = "2025/3/1"			------ Type whatever you want										Default = A date
+local show_controls = true			------ Not working?(WIP func)
+------------------------------------------------	Anti-Recoil CONFIG			--------------------------------------------------
+local mode_1_name = "Chick" 			------ Set custom names for each config you are running
+local mode_1_default_delay = 30
+local mode_2_name = "Strong"
+local mode_2_default_delay = 17
+local mode_3_name = "HARD"
+local mode_3_default_delay = 14
+local mode_4_name = "Ultra"
+local mode_4_default_delay = 10
+local mode_5_name = "Lord"
+local mode_5_default_delay = 8
+local mode_6_name = "Tachanka"
+local mode_6_default_delay = 2
+------------------------------------------------	Rapid-Fire CONFIG			--------------------------------------------------
+local mode_rapid_1_name = "AutoFire-Slow"
+local mode_rapid_1_default_delay = 20		------ Rapid-Fire Click Speed (Lower = Faster)
+local mode_rapid_1_default_Y_level = 1
+local mode_rapid_2_name = "AutoFire-Quick"
+local mode_rapid_2_default_delay = 20
+local mode_rapid_2_default_Y_level = 2
+local mode_rapid_3_name = "Pistol-Slow"
+local mode_rapid_3_default_delay = 20
+local mode_rapid_3_default_Y_level = 4
+local mode_rapid_4_name = "Pistol-Fast"
+local mode_rapid_4_default_delay = 20
+local mode_rapid_4_default_Y_level = 6
+local mode_rapid_5_name = "OTG-41-Slow"
+local mode_rapid_5_default_delay = 20
+local mode_rapid_5_default_Y_level = 9
+local mode_rapid_6_name = "OTG-41-Fast"
+local mode_rapid_6_default_delay = 20
+local mode_rapid_6_default_Y_level = 30
+------------------------------------------------		Main Module			--------------------------------------------------
+--						DONT TOUCH IF U DONT KNOW WHAT THEY'RE DOING!!!							--
+local Mode      = "Recoil"
+local Stat      = "False"
+local time
+local timer = 0
+local timerlast = 0
+local rsleep = 50
+local DELAYMAX = 100
+local power = 1
+local firstinterval = 0
+local Recoil_Step = math.random(1,11)
+local Recoil_Step1 = math.random(1,2)
+local delayint = 100
+local count = 0
+local finalcount = 7
+local firsttrigger = false
+local mode_1_delay = mode_1_default_delay
+local mode_2_delay = mode_2_default_delay
+local mode_3_delay = mode_3_default_delay
+local mode_4_delay = mode_4_default_delay
+local mode_5_delay = mode_5_default_delay
+local mode_6_delay = mode_6_default_delay
+
+ function mode(event, num) ---events: change
+    if (mode_change_enabled == true and event == "change")then
+    	if(mode_selected == 1)then
+    		mode_selected = 2
+    		Delay = mode_2_default_delay
+    		rapid_Y_level = mode_rapid_2_default_Y_level
+			rapid_firerate = mode_rapid_2_default_delay
+    	elseif(mode_selected == 2)then
+    		mode_selected = 3
+    		Delay = mode_3_default_delay
+			rapid_Y_level = mode_rapid_3_default_Y_level
+			rapid_firerate = mode_rapid_3_default_delay
+    		elseif(mode_selected == 3)then
+    		mode_selected = 4
+    		Delay = mode_4_default_delay		
+			rapid_Y_level = mode_rapid_4_default_Y_level	
+			rapid_firerate = mode_rapid_4_default_delay
+   		elseif(mode_selected == 4)then
+    		mode_selected = 5
+    		Delay = mode_5_default_delay
+			rapid_Y_level = mode_rapid_5_default_Y_level
+			rapid_firerate = mode_rapid_5_default_delay
+    	elseif(mode_selected ==5)then
+    		mode_selected = 6
+    		Delay = mode_6_default_delay
+			rapid_Y_level = mode_rapid_6_default_Y_level
+			rapid_firerate = mode_rapid_6_default_delay
+		elseif(mode_selected ==6)then
+			mode_selected = 1
+    		Delay = mode_1_default_delay
+			rapid_Y_level = mode_rapid_1_default_Y_level
+			rapid_firerate = mode_rapid_1_default_delay
+    	end
+    	log()
+    end
+end ----end func mode()
+
+function OnEvent(event, arg)
+    --OutputLogMessage("Key: ".. arg.. "\n")
+ 	if ( event == "PROFILE_ACTIVATED" ) then
+		math.randomseed(GetRunningTime()*Recoil_Step)
+		EnablePrimaryMouseButtonEvents(true)
+		ClearLog()	
+		print"its working"
+		OutputLogMessage("[[________________________________________________________\n")
+		OutputLogMessage("                                                              \n")
+		OutputLogMessage("     K2LINTECH - Universal Multi Function Anti-Recoil Script     \n")
+		OutputLogMessage("     Made by K2LinN Aka Daniel24971                                          \n")  
+		OutputLogMessage("     Function works in many FPS games!                                                      ")
+		OutputLogMessage("\n[[________________________________________________________\n")
+		OutputLogMessage("\n")
+		Sleep(100)
+		OutputLogMessage("Loading Modules     ")
+		Sleep(250)
+		ClearLog()
+		OutputLogMessage("Loading Modules.    ")
+		Sleep(250)
+		ClearLog()
+		OutputLogMessage("Loading Modules..   ")
+		Sleep(150)
+		ClearLog()
+		OutputLogMessage("Loading Modules...  ")
+		Sleep(100)
+		ClearLog()
+		OutputLogMessage("Loading Modules.... ")
+		Sleep(250)
+		ClearLog()
+		OutputLogMessage("[			#===K2LINTECH v11 ===||[_G-series Lua API _]||===#			]\n");
+		OutputLogMessage("Checking Anti-Recoil Function			--Complete				|\n")
+		Sleep(100)
+		OutputLogMessage("Checking Rapid-Fire Function			--Complete				|\n")
+		Sleep(100)
+		OutputLogMessage("Checking Synchronization Function			--Complete				|\n")
+		Sleep(100)
+		OutputLogMessage("Checking Config Function				--Complete				|\n")
+		Sleep(100)
+		OutputLogMessage("Checking Loader Function				--Complete				|\n")
+		Sleep(100)
+		OutputLogMessage(" 			All Checked! Scripts ready to go!				|\n")
+		OutputLogMessage("[#			===||[_ Made  by     K2Lin24971_]||===				#\n");
+		Sleep(150)
+		ClearLog()
+		log()
+		end
+		
+	if ( event == "MOUSE_BUTTON_PRESSED" and arg == modeswitch) then
+		if ( Recoil ) then
+			Recoil    = false
+			Automatic = true
+			Mode      = "Rapid-Fire & Anti-Recoil"
+			log()
+		elseif ( Automatic ) then
+			Recoil    = true
+			Automatic = false
+			Mode      = "Anti-Recoil"
+			log()
+		else
+	end
+		
+	elseif ( event == "MOUSE_BUTTON_PRESSED" and arg == masterswitch) then
+		if ( Master ) then
+			Master    = false
+			log()
+		else
+			Master    = true
+			log()
+		end
+	end
+
+	if (event == "MOUSE_BUTTON_PRESSED" and arg == mode_change_button) then
+		mode("change")
+		log()
+		end
+		
+	if (event == "MOUSE_BUTTON_PRESSED" and arg == reset_button) then
+		mode_selected = 1
+    	Delay = mode_1_default_delay
+		rapid_Y_level = mode_rapid_1_default_Y_level
+		rapid_firerate = mode_rapid_1_default_delay
+		log()
+		end
+
+	power = math.floor(((1)/(Delay)) * (10^3) + 0.5)/(10^3)
+		if (event == "MOUSE_BUTTON_PRESSED") then
+			OutputLogMessage("You pressed Mouse-G")
+			OutputLogMessage(arg)
+		elseif(event == "MOUSE_BUTTON_RELEASED") then
+			OutputLogMessage("You released Mouse-G")
+			OutputLogMessage(arg)
+		end
+ 
+	if(event == "MOUSE_BUTTON_PRESSED" and Recoil) then
+		firing (0,Y_level,Delay) --Calls function
+		log()
+	elseif(event == "MOUSE_BUTTON_PRESSED" and arg == rapid_button and Automatic) then
+		firing (0,rapid_Y_level,rapid_Delay) --Calls Rapid function
+		log()
+end
+end---end onevent()
+
+
+function log()
+	if(not IsMouseButtonPressed(1) or not IsMouseButtonPressed(3))then
+		ClearLog()
+		OutputLogMessage("#=========K2LINTECH MENU =========||[_G-series Lua API_]||=========#\n");
+		OutputLogMessage("[G");
+		OutputLogMessage(masterswitch);
+		OutputLogMessage("] MainSwitch:");
+		OutputLogMessage(tostring(Master));
+		OutputLogMessage("  [G");
+		OutputLogMessage(modeswitch);
+		OutputLogMessage("] FunctionMode:");
+		OutputLogMessage(Mode);
+		OutputLogMessage(" , Delay(ms) = ");
+		OutputLogMessage(Delay);
+		OutputLogMessage("/");
+		OutputLogMessage(DELAYMAX);
+		OutputLogMessage("\n")
+		OutputLogMessage("Power(Work/Time) = ");
+		OutputLogMessage(power)
+		OutputLogMessage(" ")
+		end
+	if(debug == true)then
+		cps = 1000 / rapid_firerate
+		OutputLogMessage(", Recoil_Step = ");
+		OutputLogMessage(Recoil_Step)
+		OutputLogMessage(", Recoil_Step1 = ");
+		OutputLogMessage(Recoil_Step1)
+		OutputLogMessage(", count = ");
+		OutputLogMessage(count)
+		OutputLogMessage(" \n")
+		OutputLogMessage("Config_Loaded_# = ");
+		OutputLogMessage(mode_selected.." , ")
+		OutputLogMessage("Rapid-Fire Recoil : ")
+		OutputLogMessage(rapid_Y_level)
+		OutputLogMessage(" , ")
+		OutputLogMessage("Click :")
+		OutputLogMessage(cps)
+		OutputLogMessage(" Click Per Secound \n")
+		OutputLogMessage("[G"..mode_change_button.."]  Mode_change_button = ");
+		OutputLogMessage(tostring(mode_change_enabled));
+		OutputLogMessage(", debug = ");
+		OutputLogMessage(tostring(debug))
+		OutputLogMessage(", mode_change_enabled = ");
+		OutputLogMessage(tostring(mode_change_enabled))
+		OutputLogMessage(", Y_level = ");
+		OutputLogMessage(Y_level.."\n")
+		end
+	if(mode_change_enabled==true)then
+    	if(mode_selected == 1)then
+    		OutputLogMessage("[ (# "..mode_1_name.." # - "..mode_1_delay.." / "..DELAYMAX.." ms) ]"); OutputLogMessage("	(# "..mode_2_name.." # - "..mode_2_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	(# "..mode_3_name.." # - "..mode_3_delay.." / "..DELAYMAX.." ms)");  OutputLogMessage("	(# "..mode_4_name.." # - "..mode_4_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	(# "..mode_5_name.." # - "..mode_5_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	(# "..mode_6_name.." # - "..mode_6_delay.." / "..DELAYMAX.." ms)");
+    	elseif(mode_selected == 2)then
+    		OutputLogMessage("(# "..mode_1_name.." # - "..mode_1_delay.." / "..DELAYMAX.." ms)	"); OutputLogMessage("[ (# "..mode_2_name.." # - "..mode_2_delay.." / "..DELAYMAX.." ms) ]"); OutputLogMessage("	(# "..mode_3_name.." # - "..mode_3_delay.." / "..DELAYMAX.." ms)");   OutputLogMessage("	(# "..mode_4_name.." # - "..mode_4_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	(# "..mode_5_name.." # - "..mode_5_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	(# "..mode_6_name.." # - "..mode_6_delay.." / "..DELAYMAX.." ms)");
+    	elseif(mode_selected == 3)then
+    		OutputLogMessage("(# "..mode_1_name.." # - "..mode_1_delay.." / "..DELAYMAX.." ms)	"); OutputLogMessage("(# "..mode_2_name.." # - "..mode_2_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	[ (# "..mode_3_name.." # - "..mode_3_delay.." / "..DELAYMAX.." ms) ]");   OutputLogMessage("	(# "..mode_4_name.." # - "..mode_4_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	(# "..mode_5_name.." # - "..mode_5_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	(# "..mode_6_name.." # - "..mode_6_delay.." / "..DELAYMAX.." ms)");
+		elseif(mode_selected == 4)then
+    		OutputLogMessage("(# "..mode_1_name.." # - "..mode_1_delay.." / "..DELAYMAX.." ms)	"); OutputLogMessage("(# "..mode_2_name.." # - "..mode_2_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	(# "..mode_3_name.." # - "..mode_3_delay.." / "..DELAYMAX.." ms)");   OutputLogMessage("	[ (# "..mode_4_name.." # - "..mode_4_delay.." / "..DELAYMAX.." ms) ]"); OutputLogMessage("	(# "..mode_5_name.." # - "..mode_5_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	(# "..mode_6_name.." # - "..mode_6_delay.." / "..DELAYMAX.." ms)");
+    	elseif(mode_selected == 5)then
+    		OutputLogMessage("(# "..mode_1_name.." # - "..mode_1_delay.." / "..DELAYMAX.." ms)	"); OutputLogMessage("(# "..mode_2_name.." # - "..mode_2_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	(# "..mode_3_name.." # - "..mode_3_delay.." / "..DELAYMAX.." ms)");   OutputLogMessage("	(# "..mode_4_name.." # - "..mode_4_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	[ (# "..mode_5_name.." # - "..mode_5_delay.." / "..DELAYMAX.." ms) ]"); OutputLogMessage("	(# "..mode_6_name.." # - "..mode_6_delay.." / "..DELAYMAX.." ms)");
+    	elseif(mode_selected == 6)then
+    		OutputLogMessage("(# "..mode_1_name.." # - "..mode_1_delay.." / "..DELAYMAX.." ms)	"); OutputLogMessage("(# "..mode_2_name.." # - "..mode_2_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	(# "..mode_3_name.." # - "..mode_3_delay.." / "..DELAYMAX.." ms)");   OutputLogMessage("	(# "..mode_4_name.." # - "..mode_4_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	(# "..mode_5_name.." # - "..mode_5_delay.." / "..DELAYMAX.." ms)"); OutputLogMessage("	[ (# "..mode_6_name.." # - "..mode_6_delay.." / "..DELAYMAX.." ms) ]");
+		end
+	OutputLogMessage("\n")
+	OutputLogMessage("-----Clantag-K2LINGROUP BETA V11 	ReleseDate:"..ReleseDate.."-----\n")
+	end
+end
+
+function firing(a,b,rsleep)
+    if (Master and Recoil) then
+		if IsMouseButtonPressed(3) then
+			if(IsMouseButtonPressed(1)) then
+				repeat
+					Recoil_Step1 = math.random(1,2)
+					if (Recoil == true) then
+					if(x_composite_enabled) then
+						x_rec = math.random(-3,1)
+						MoveMouseRelative(x_rec,0)
+					end
+					if(autotag) then
+						PressKey(tagbutton)
+						Sleep(math.random(5,25))
+						ReleaseKey(tagbutton)
+						Sleep(math.random(5,25))
+					end
+						time = GetRunningTime()
+							if(Delay >= 3)then
+							if(Recoil_Step == 1)then
+								MoveMouseRelative(a,b)
+								Sleep(rsleep)
+								Recoil_Step = math.random(1,7)
+							elseif(Recoil_Step == 2)then
+								MoveMouseRelative(a,b)
+								Sleep(rsleep+2)
+								Recoil_Step = math.random(3,7)
+							elseif(Recoil_Step == 3)then
+								MoveMouseRelative(a,b)
+								Sleep(rsleep+2)
+								Recoil_Step = math.random(4,7)
+							elseif(Recoil_Step == 4)then
+								MoveMouseRelative(a,b)
+								Sleep(rsleep+1)
+								Recoil_Step = math.random(5,7)
+							elseif(Recoil_Step == 5)then
+								MoveMouseRelative(a,b)
+								Sleep(rsleep)
+								Recoil_Step = math.random(1,4)
+							elseif(Recoil_Step == 6)then
+								MoveMouseRelative(a,b)
+								Sleep(rsleep-2)
+								Recoil_Step = math.random(2,5)
+							elseif(Recoil_Step == 7)then
+								MoveMouseRelative(a,b)
+								Sleep(rsleep+4)
+								Recoil_Step = math.random(3,6)
+							elseif(Recoil_Step == 8)then
+								MoveMouseRelative(a,b)
+								Sleep(rsleep+1)
+								Recoil_Step = math.random(5,11)
+							elseif(Recoil_Step == 9)then
+								MoveMouseRelative(a,b)
+								Sleep(rsleep)
+								Recoil_Step = math.random(8,11)
+							elseif(Recoil_Step == 10)then
+								MoveMouseRelative(a,b)
+								Sleep(rsleep-2)
+								Recoil_Step = math.random(9,12)
+							elseif(Recoil_Step == 11)then
+								MoveMouseRelative(a,b)
+								Sleep(rsleep+1)
+								Recoil_Step = math.random(3,14)
+							end
+							else
+								MoveMouseRelative(a,b)
+								Sleep(rsleep-1)
+								Recoil_Step = math.random(1,11)
+							end
+						timer = GetRunningTime()-time
+						if(timer >= timerlast)then 
+							Recoil_Step1 = math.random(1,2)
+						end
+						ClearLog()
+						OutputLogMessage("Function Cycle runs in : ")
+						OutputLogMessage(timer)
+						OutputLogMessage(" millisecond(s)\n")
+					end
+				until (not IsMouseButtonPressed(1) or not IsMouseButtonPressed(3))
+				firstinterval = 0
+				timerlast = timer
+			end
+		end
+    elseif (Master and Automatic) then
+		if IsMouseButtonPressed(3) then
+			if IsMouseButtonPressed(rapid_button) then
+				repeat
+					Recoil_Step1 = math.random(1,2)
+					if (Automatic) then
+					PressKey(autofirekey)
+					if(autotag) then
+					PressKey(tagbutton)
+					Sleep(math.random(5,25))
+					ReleaseKey(tagbutton)
+					Sleep(math.random(5,25))
+					end
+						time = GetRunningTime()
+							if(Delay >= 3)then
+							if(Recoil_Step == 1)then
+								MoveMouseRelative(a,b-2)
+								Sleep(Delay)
+								Recoil_Step = math.random(1,7)
+							elseif(Recoil_Step == 2)then
+								MoveMouseRelative(a,b-1)
+								Sleep(Delay)
+								Recoil_Step = math.random(3,7)
+							elseif(Recoil_Step == 3)then
+								MoveMouseRelative(a,b)
+								Sleep(Delay-1)
+								Recoil_Step = math.random(4,7)
+							elseif(Recoil_Step == 4)then
+								MoveMouseRelative(a,b+3)
+								Sleep(Delay-1)
+								Recoil_Step = math.random(5,7)
+							elseif(Recoil_Step == 5)then
+								MoveMouseRelative(a,b+5)
+								Sleep(Delay)
+								Recoil_Step = math.random(1,4)
+							elseif(Recoil_Step == 6)then
+								MoveMouseRelative(a,b+2)
+								Sleep(Delay+2)
+								Recoil_Step = math.random(2,5)
+							elseif(Recoil_Step == 7)then
+								MoveMouseRelative(a,b-2)
+								Sleep(Delay-1)
+								Recoil_Step = math.random(3,6)
+							elseif(Recoil_Step == 8)then
+								MoveMouseRelative(a,b+5)
+								Sleep(Delay)
+								Recoil_Step = math.random(3,11)
+							elseif(Recoil_Step == 9)then
+								MoveMouseRelative(a,b+2)
+								Sleep(Delay+1)
+								Recoil_Step = math.random(2,9)
+							elseif(Recoil_Step == 10)then
+								MoveMouseRelative(a,b-2)
+								Sleep(Delay-1)
+								Recoil_Step = math.random(1,6)
+							elseif(Recoil_Step == 11)then
+								MoveMouseRelative(a,b-2)
+								Sleep(Delay+2)
+								Recoil_Step = math.random(3,14)
+							end
+							else
+								MoveMouseRelative(a,b+1)
+								Sleep(Delay-1)
+								Recoil_Step = math.random(1,11)
+							end
+						timer = GetRunningTime()-time
+						if(timer >= timerlast)then
+							Recoil_Step1 = math.random(1,2)
+						end
+					Sleep(rapid_delay)
+					ReleaseKey(autofirekey)
+					Sleep(rapid_firerate)
+						ClearLog()
+						OutputLogMessage("Function Cycle runs in : ")
+						OutputLogMessage(timer+rapid_firerate)
+						OutputLogMessage(" millisecond(s)\n")
+					end
+				until (not IsMouseButtonPressed(rapid_button) or not IsMouseButtonPressed(3))
+				firstinterval = 0
+				timerlast = timer
+				end
+			end
+		end --END AT HERE!
+	end
